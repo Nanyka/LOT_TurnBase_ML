b8f576b258a8bd9c1495f14d192dd9d0a6bab617
BinhLai_AD0223 <thanhbinh4388@gmail.com>
2023-04-27 15:33:06 +0300

    Return To upgrade Unity 2022

---
+++ Assets/[MAIN]LOT_Turnbase/TestClasses/ITileInfo.cs
@@ -0,0 +1,7 @@
+namespace LOT_Turnbase
+{
+    public interface ITileAction
+    {
+        public void Spawn();
+    }
+}
---
+++ Assets/[MAIN]LOT_Turnbase/TestClasses/TileInfo.cs
@@ -0,0 +1,7 @@
+namespace LOT_Turnbase
+{
+    public class TileInfo<T>
+    {
+        private T _info;
+    }
+}
